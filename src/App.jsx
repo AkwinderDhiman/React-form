3503d295523dfaf1a8987775bbc349669d1596bf
AkwinderDhiman <akwinder128@gmail.com>
2024-10-01 16:11:24 +0530

    get form data using useState

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header/Header.jsx";
+import Login from "./Components/Login/Login.jsx";
+
+function App() {
+  return (
+    <>
+       <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./Login.css";
+
+export default function Login() {
+  const [enteredValue, setEnteredValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleInputChange(identifier, value) {
+    setEnteredValue((prev) => ({
+      ...prev,
+      [identifier]: value,
+    }));
+  }
+
+  function handleSubmitForm(event) {
+    event.preventDefault();
+    console.log("submitForm", enteredValue);
+  }
+
+  return (
+    <form onSubmit={handleSubmitForm}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValue.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event.target.value)}
+            value={enteredValue.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
